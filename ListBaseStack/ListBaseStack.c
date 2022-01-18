462d39b9d27ce46e456c50d10e9535b5e7f03570
nash4826 <gys4826@gmail.com>
2022-01-18 21:44:20 +0900

    ListBaseStack

---
+++ ListBaseStack/ListBaseStack.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ListBaseStack.h"
+
+void StackInIt(Stack* pstack) {
+	pstack->head = NULL;
+}
+
+int SIsEmpty(Stack* pstack) {
+	if (pstack->head == NULL)
+		return TRUE;
+	else
+		return FALSE;
+}
+
+void SPush(Stack* pstack, Data data) {
+	Node* newNode = malloc(sizeof(Node));
+	newNode->data = data;
+
+	newNode->next = pstack->head;
+	pstack->head = newNode;
+
+}
+
+Data SPop(Stack* pstack) {
+	if (SIsEmpty(&pstack))
+	{
+		printf("Stack Empty\n");
+		exit(-1);
+	}
+	Node* rpos = pstack->head;
+	Data rdata = rpos->data;
+
+	pstack->head = pstack->head->next;
+	
+	free(rpos);
+	return rdata;
+}
+
+Data SPeek(Stack* pstack) {
+	return pstack->head->data;
+}
