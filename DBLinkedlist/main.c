41e6dab61d333e54f0cbe4b3d920899b19c66f1b
nash4826 <gys4826@gmail.com>
2022-01-18 20:04:22 +0900

    DBLinkedList

---
+++ DBLinkedlist/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "DBLinkedList.h"
+
+int main()
+{
+	List list;
+	int data;
+	ListInit(&list);
+
+	LInsert(&list, 1);
+	LInsert(&list, 2);
+	LInsert(&list, 3);
+	LInsert(&list, 4);
+	LInsert(&list, 5);
+
+	if (LFirst(&list, &data)) {
+		printf("%d ", data);
+		while (LNext(&list, &data))
+			printf("%d ", data);
+
+		while (LPrevious(&list, &data))
+			printf("%d ", data);
+
+		printf("\n\n");
+	}
+}
