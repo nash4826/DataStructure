e426c8cc23b25c28ab08f985ef7fa46519cda89b
nash4826 <gys4826@gmail.com>
2022-01-19 16:30:33 +0900

    Calculator Part2

---
+++ PostCalculator/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "PostCalculator.h"
+
+int main()
+{
+	char exp1[] = "42*8+";
+	char exp2[] = "123+*4/";
+	
+
+	printf("%s = %d\n", exp1,EvalRPNExp(exp1));
+	printf("%s = %d\n", exp2,EvalRPNExp(exp2));
+
+	return 0;
+}
