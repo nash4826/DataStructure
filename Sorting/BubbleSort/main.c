a6d15b7f478e76a285c3bdf31b4fe4b2a8f16fc2
nash4826 <gys4826@gmail.com>
2022-02-19 22:00:02 +0900

    Add Sorting(BubbleSort)

---
+++ Sorting/BubbleSort/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/*
+	*버블 소트*
+	
+	시간복잡도 O(n^2)
+	
+*/
+
+void bubble_sort(int* arr, int size)
+{
+	int temp;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+
+
+int main()
+{
+	int arr[10] = { 7,2,6,1,5,3,10,4,9,8 };
+	bubble_sort(arr, sizeof(arr) / sizeof(int));
+	
+	for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+	{
+		printf("%d ", arr[i]);
+	}
+
+
+	return 0;
+}
